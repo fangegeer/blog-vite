910a170865c03b43f740786320595e558254b879
chenf <2504059162@qq.com>
2025-10-10 09:45:48 +0800

    feat(docs): 更新sitemap配置以支持视频扩展
    - 在sitemap中添加Google视频扩展命名空间
    -保留现有页面URL映射逻辑- 维持XML格式兼容性

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -55,8 +55,9 @@ export default {
       })
 
       // 生成 sitemap XML
-      const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemap.org/schemas/sitemap/0.9">
+      const sitemap = `<?xml version="1.0" encoding="UTF-8"?> 
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+      xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 ${pages.map(page => `
   <url>
     <loc>${page.loc}</loc>
